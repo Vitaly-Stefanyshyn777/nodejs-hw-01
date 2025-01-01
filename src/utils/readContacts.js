824f9c9bf4ee3d11c52ba2f2332ae2ac0329201e
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-01-01 18:56:38 +0200

    Крок 2

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from "../constants/contacts.js";
+import fs from "node:fs/promises";
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, { encoding: "utf-8" });
+
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading contacts:", error.message);
+    throw new Error("Failed to read contacts");
+  }
+};
