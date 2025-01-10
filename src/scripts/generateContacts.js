00824c1b5edf6583e3aec19a6dd3da5a256cacbe
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-01-10 21:18:45 +0200

    підкоригував

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,15 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+const generateContacts = async (number) => {
+    try {
+        const generateUsers = [];
+        for (let i = 0; i <= number; i += 1) {
+            const generate = generateContacts()
+            generateUsers.push(generate)
+        }
+        await writeContacts(generateUsers)
+}
+};
 
 generateContacts(5);
