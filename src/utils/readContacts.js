c2a7a5481de3ea2ca40713b6060c60a90d65960b
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-01-14 21:51:08 +0200

    почистив

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -11,27 +11,3 @@ export const readContacts = async () => {
     throw new Error("Failed to read contacts");
   }
 };
-
-// import { PATH_DB } from "../constants/contacts.js";
-// import fs from "node:fs/promises";
-
-// export const readContacts = async () => {
-//   try {
-//     const data = await fs.readFile(PATH_DB, { encoding: "utf-8" });
-
-//     if (!data) {
-//       console.warn("File is empty. Returning an empty array.");
-//       return [];
-//     }
-
-//     return JSON.parse(data);
-//   } catch (error) {
-//     if (error.code === "ENOENT") {
-//       console.error("File not found. Returning an empty array.");
-//       return [];
-//     }
-
-//     console.error("Error reading contacts:", error.message);
-//     throw new Error("Failed to read contacts");
-//   }
-// };
